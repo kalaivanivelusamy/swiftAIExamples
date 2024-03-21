6551fddcb130e2cf6de0f0824b19c63698675483
kayvee <kalai.v.vani@gmail.com>
2024-03-21 12:53:12 +0100

    initial commit

---
+++ SwiftAIExamples/SwiftAIExamples/SwiftAIExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAIExamplesApp.swift
+//  SwiftAIExamples
+//
+//  Created by kalaivani on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAIExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
